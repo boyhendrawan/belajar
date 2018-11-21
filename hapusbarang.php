57ad3da94b476eca015150f3e05e4fea6a04fc2f
boyhendrawan <boyhendrawanpurba@gmail.com>
2018-11-21 14:14:34 +0700

    file di hapus

--- hapusbarang.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	session_start();
-	$ID_BUKU=$_GET['id'];
-	unset($_SESSION['keranjang'][$ID_BUKU]);
-
-	echo "<script>alert('Barang Berhasil Di Hapus')</script>";
-	echo "<script>location='keranjang.php';</script>";
-	
- ?>
